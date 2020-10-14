6993f94d5b2383b919f369c8ae405070d33598a1
Gabriel Bohn <gabrielcbohn@gmail.com>
2020-10-14 13:50:50 -0300

    Done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import "express-async-errors";
+import path from "path";
+import cors from "cors";
+
 import "./database/connection";
 
 import routes from "./routes";
+import errorHandler from "./errors/handler";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
 //Query params http://localhost:3333/users?search=diego possivel concatenar com & ex: http://localhost:3333/users?search=diego&nome=diego
 //Route params http://localhost:3333/users/diego
