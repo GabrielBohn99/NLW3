72ecf3ee25b00269a45ac07de2a82f3a5cfd999f
Gabriel Bohn <gabrielcbohn@gmail.com>
2020-10-14 12:08:38 -0300

    Database setup, routes file created and controllers done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import "./database/connection";
+
+import routes from "./routes";
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    
-})
+app.use(express.json());
+app.use(routes);
+
+//Query params http://localhost:3333/users?search=diego possivel concatenar com & ex: http://localhost:3333/users?search=diego&nome=diego
+//Route params http://localhost:3333/users/diego
 
 app.listen(3333);
